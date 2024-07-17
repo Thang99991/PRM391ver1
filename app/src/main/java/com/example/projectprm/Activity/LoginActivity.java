423fe588eaa7ed9ceefae0de3e91984ff64ae063
thang99991 <dohongthang258@gmail.com>
2024-07-17 20:00:42 +0700

    call api login

--- app/src/main/java/com/example/projectprm/Activity/LoginActivity.java
+++ app/src/main/java/com/example/projectprm/Activity/LoginActivity.java
@@ -2,29 +2,111 @@ package com.example.projectprm.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.projectprm.Adapter.ProductCartAdapter;
+import com.example.projectprm.Api.ApiService;
+import com.example.projectprm.Api.RetrofitClient;
+import com.example.projectprm.Model.Account;
+import com.example.projectprm.Model.LoginResponse;
+import com.example.projectprm.Model.ProductCart;
 import com.example.projectprm.R;
 
+import java.io.Serializable;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class LoginActivity extends AppCompatActivity {
+    EditText Email, Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
 
+        Email = findViewById(R.id.email);
+        Password = findViewById(R.id.password);
+
         Button loginButton = findViewById(R.id.sign_in_button);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Chuyển đến MainActivity khi người dùng nhấn nút đăng nhập
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                validateAndLogin();
+
             }
         });
     }
+
+    private void validateAndLogin() {
+        String email = Email.getText().toString().trim();
+        String password = Password.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Email.setError("Email is required");
+            Password.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Email.setError("Invalid email format");
+            Email.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Password.setError("Password is required");
+            Password.requestFocus();
+            return;
+        }
+
+        // Perform login action (e.g., send a login request to your server)
+        LoginAccount(email, password);
+    }
+
+    private void LoginAccount(String Email, String Password) {
+        try {
+            ApiService apiService = RetrofitClient.getRetrofitInstance().create(ApiService.class);
+            Account account = new Account(Email, Password);
+
+            Call<LoginResponse> call = apiService.checkLogin(account);
+
+            call.enqueue(new Callback<LoginResponse>() {
+                @Override
+                public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                    if (response.isSuccessful() && response.body() != null) {
+                        Account account1 = response.body().getUser();
+
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.putExtra("account", (Serializable) account1);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "No Account available", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<LoginResponse> call, Throwable t) {
+                    Toast.makeText(LoginActivity.this, "Failed to Login Account", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (Exception e) {
+            Log.d("Loi", e.getMessage());
+        }
+
+
+    }
+
 }
